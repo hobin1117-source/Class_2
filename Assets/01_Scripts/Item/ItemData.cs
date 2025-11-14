700bfbce2e722ac34d8915f0c14af5b30c1eebbe
hyeonwooJ <janghw4116@gmail.com>
2025-11-14 17:40:15 +0900

    feat: 플레이어 초기 세팅
    플레이어 초기 세팅

---
+++ Assets/01_Scripts/Item/ItemData.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable,
+    ReSource
+}
+
+public enum ConsumableType
+{
+    Health,
+    Hunger
+}
+
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefabs;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmout;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] cosumables;
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
+}
---
+++ Assets/01_Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damage);
+
+}
+
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UICondition uICondition;
+
+    Condition health { get { return uICondition.health; } }
+    Condition hunger { get { return uICondition.hunger; } }
+    Condition stamina { get { return uICondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    public event Action onTakeDamage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue == 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+        if (health.curValue == 0f)
+        {
+            Die();
+        }
+
+    }
+    public void Heal(float amout)
+    {
+        health.Add(amout);
+    }
+    public void Eat(float amout)
+    {
+        hunger.Add(amout);
+    }
+
+    public void Die()
+    {
+        Debug.Log("ав╬З╢ы.");
+    }
+
+    public void TakePhysicalDamage(int damage)
+    {
+        health.Subtract(damage);
+        onTakeDamage?.Invoke();
+    }
+
+    public bool UseStamina(float amount)
+    {
+        if (stamina.curValue - amount < 0f)
+        {
+            return false;
+        }
+        stamina.Subtract(amount);
+        return true;
+    }
+}
